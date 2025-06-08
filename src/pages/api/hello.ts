952aa9d151ee368a8065f5da0d44ead961ec997a
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-08 12:53:11 -0700

    Set up Prisma and database (#2)

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@/generated/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
-};
+  id: number;
+  title: string;
+  content: string | null;
+  published: boolean;
+  authorId: number | null;
+}[];
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const prisma = new PrismaClient();
+  const posts = await prisma.post.findMany({ take: 10 });
+  res.status(200).json(posts);
 }
