10f5cbf40264638038eef591709d529166567345
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-10 15:25:45 -0700

    Add response type to GET /instructors/{id}

--- src/pages/api/instructors/[id].ts
+++ src/pages/api/instructors/[id].ts
@@ -1,7 +1,9 @@
+import { Instructor } from "@/generated/prisma";
 import { makeApiHandler, prisma } from "@/lib";
+import { NextApiResponse } from "next";
 
 export default makeApiHandler({
-  GET: async (req, res) => {
+  GET: async (req, res: NextApiResponse<Instructor>) => {
     const id = Number(req.query.id as string);
     const instructor = await prisma.instructor.findUnique({ where: { id } });
     if (!instructor) {
