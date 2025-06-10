602abe56717bcc3e24cec263f6842b32b4a24e32
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-10 16:18:17 -0700

    Add API route to delete an instructor

--- src/pages/api/instructors/[id].ts
+++ src/pages/api/instructors/[id].ts
@@ -7,6 +7,7 @@ export default makeApiHandler({
     const id = Number(req.query.id as string);
     if (isNaN(id)) {
       res.status(400).end();
+      return;
     }
     const instructor = await prisma.instructor.findUnique({ where: { id } });
     if (!instructor) {
@@ -32,6 +33,26 @@ export default makeApiHandler({
         where: { id },
         data: { name },
       });
+      res.status(200).json(instructor);
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2025") {
+          res.status(404).end();
+          return;
+        }
+      }
+      res.status(500).end();
+    }
+  },
+
+  DELETE: async (req, res: NextApiResponse<Instructor>) => {
+    const id = Number(req.query.id as string);
+    if (isNaN(id)) {
+      res.status(400).end();
+      return;
+    }
+    try {
+      const instructor = await prisma.instructor.delete({ where: { id } });
       res.status(200).json(instructor);
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
