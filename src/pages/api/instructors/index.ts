e5691881b05796d15b541fc93870db0f09d38fef
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-10 14:58:43 -0700

    Add API route to create an instructor

--- src/pages/api/instructors/index.ts
+++ src/pages/api/instructors/index.ts
@@ -1,12 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiResponse } from "next";
 import { Instructor } from "@/generated/prisma";
 import { makeApiHandler } from "@/lib/makeApiHandler";
 import { prisma } from "@/lib/prisma";
 
 const handler = makeApiHandler({
-  GET: async (req: NextApiRequest, res: NextApiResponse<Instructor[]>) => {
+  GET: async (req, res: NextApiResponse<Instructor[]>) => {
     const instructors = await prisma.instructor.findMany();
     res.status(200).json(instructors);
+  },
+
+  POST: async (req, res: NextApiResponse<Instructor>) => {
+    const { name } = req.body;
+    if (!name) {
+      res.status(400).end();
+      return;
+    }
+    const instructor = await prisma.instructor.create({ data: { name } });
+    res.status(201).json(instructor);
   },
 });
 
