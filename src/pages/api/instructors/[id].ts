2c1f17cf736a53dc3e3df246838aea0fdec0cdca
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-10 16:01:24 -0700

    Add API route to update an instructor

--- src/pages/api/instructors/[id].ts
+++ src/pages/api/instructors/[id].ts
@@ -1,15 +1,46 @@
-import { Instructor } from "@/generated/prisma";
+import { Instructor, Prisma } from "@/generated/prisma";
 import { makeApiHandler, prisma } from "@/lib";
 import { NextApiResponse } from "next";
 
 export default makeApiHandler({
   GET: async (req, res: NextApiResponse<Instructor>) => {
     const id = Number(req.query.id as string);
+    if (isNaN(id)) {
+      res.status(400).end();
+    }
     const instructor = await prisma.instructor.findUnique({ where: { id } });
     if (!instructor) {
       res.status(404).end();
       return;
     }
     res.status(200).json(instructor);
+  },
+
+  PUT: async (req, res: NextApiResponse<Instructor>) => {
+    const id = Number(req.query.id as string);
+    if (isNaN(id)) {
+      res.status(400).end();
+      return;
+    }
+    const { name } = req.body;
+    if (!name) {
+      res.status(400).end();
+      return;
+    }
+    try {
+      const instructor = await prisma.instructor.update({
+        where: { id },
+        data: { name },
+      });
+      res.status(200).json(instructor);
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2025") {
+          res.status(404).end();
+          return;
+        }
+      }
+      res.status(500).end();
+    }
   },
 });
