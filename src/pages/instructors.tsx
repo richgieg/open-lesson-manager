2affe1c6559599b00303e45471851db96fbf1847
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-11 11:34:15 -0700

    Add form to create a new instructor

--- src/pages/instructors.tsx
+++ src/pages/instructors.tsx
@@ -1,8 +1,9 @@
 import { Instructor } from "@/types";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 export default function Instructors() {
   const [instructors, setInstructors] = useState<Instructor[] | null>();
+  const [name, setName] = useState("");
 
   useEffect(() => {
     const fetchInstructors = async () => {
@@ -17,11 +18,29 @@ export default function Instructors() {
     return <>Loading...</>;
   }
 
+  const handleCreate = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await fetch("/api/instructors", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name }),
+    });
+    const data = await response.json();
+    setInstructors([...instructors, data]);
+    setName("");
+  };
+
   return (
     <>
       {instructors.map((instructor) => (
         <div key={instructor.id}>{instructor.name}</div>
       ))}
+      <form onSubmit={handleCreate}>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button type="submit">Create</button>
+      </form>
     </>
   );
 }
