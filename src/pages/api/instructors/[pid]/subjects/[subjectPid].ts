9d405337ad717b40b38a317b7335f73c13a99530
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-13 11:49:57 -0700

    Add API routes for instructor subject links

---
+++ src/pages/api/instructors/[pid]/subjects/[subjectPid].ts
@@ -0,0 +1,55 @@
+import type { NextApiResponse } from "next";
+import { Subject } from "@/generated/prisma";
+import { makeApiHandler, prisma, sendError } from "@/lib";
+
+export default makeApiHandler({
+  POST: async (req, res: NextApiResponse<Subject>) => {
+    const instructorPid = req.query.pid as string;
+    const subjectPid = req.query.subjectPid as string;
+    if (!subjectPid) {
+      return sendError(res, 400);
+    }
+    const instructor = await prisma.instructor.findUnique({
+      where: { pid: instructorPid },
+      select: { id: true },
+    });
+    const subject = await prisma.subject.findUnique({
+      where: { pid: subjectPid },
+    });
+    if (!instructor || !subject) {
+      return sendError(res, 404);
+    }
+    await prisma.instructor.update({
+      where: { id: instructor.id },
+      data: {
+        subjects: { connect: { id: subject.id } },
+      },
+    });
+    return res.status(200).json(subject);
+  },
+
+  DELETE: async (req, res: NextApiResponse<Subject>) => {
+    const instructorPid = req.query.pid as string;
+    const subjectPid = req.query.subjectPid as string;
+    if (!subjectPid) {
+      return sendError(res, 400);
+    }
+    const instructor = await prisma.instructor.findUnique({
+      where: { pid: instructorPid },
+      select: { id: true },
+    });
+    const subject = await prisma.subject.findUnique({
+      where: { pid: subjectPid },
+    });
+    if (!instructor || !subject) {
+      return sendError(res, 404);
+    }
+    await prisma.instructor.update({
+      where: { id: instructor.id },
+      data: {
+        subjects: { disconnect: { id: subject.id } },
+      },
+    });
+    return res.status(200).json(subject);
+  },
+});
